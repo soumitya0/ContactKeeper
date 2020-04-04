8b35daf5a2077158127d50fbc2cf5165a3d508d9
soumitya <soumityachauhan@gmail.com>
2020-04-04 15:37:31 +0530

    DB connection

--- config/db.js
+++ config/db.js
@@ -1,4 +1,44 @@
-const mongoose = require('mongoose');
-const config=require('config');
+const config = require('config');
 
-const db =  config.get(mongoURI)
+const mongoose = require('mongoose');
+
+const db = config.get('URL');
+
+
+/************ without Aysnc ******** */
+// const connectDB  = () => {
+
+//     mongoose.connect(db, {
+//         useNewUrlParser:true,
+//         useCreateIndex: true,
+//         useFindAndModify:false
+//     }).then(()=>{console.log('MongoDb Connected')})
+//     .catch(err => {
+//         console.error(err.message);
+//         process.exit(1)
+//     })
+// }
+
+
+/************ with Aysnc ******** */
+const connectDB  = async () => {
+
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser:true,
+            useCreateIndex: true,
+            useFindAndModify:false
+        });
+
+        console.log ('MongoDb connected ...')
+    } catch (err) {
+
+        console.error(err.message);
+        process.exit(1)
+        
+    }
+    
+}
+
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express= require('express');
 
+
+const connectDB = require('./config/db');
+
+
+
 const app = express();
 const PORT=process.env.PORT || 5000;
+
+
+//Connected DB 
+connectDB();
+
 
 app.get('/', (req,res) => res.json({
     msg:"welcome to the JSON RESPONSE"
